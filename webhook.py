e6837379d6ffec8f05a414c2a4300d9d6a41d063
alkahfi23 <58243832+alkahfi23@users.noreply.github.com>
2025-05-25 18:35:46 +0700

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
 BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+PORT = os.getenv("PORT")
 
 client = Client(BINANCE_API_KEY, BINANCE_API_SECRET)
 last_request_time = defaultdict(float)
@@ -135,5 +136,5 @@ def webhook():
 
 # === RUN APP ===
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # disarankan port 5000
+    port = int(os.environ.get("PORT", 8080))  # disarankan port 5000
     app.run(host="0.0.0.0", port=port)
